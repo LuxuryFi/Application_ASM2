a7c74809c610718c8662edb22503a8f893b75a91
Nguyễn Quốc Anh <65543110+LuxuryFi@users.noreply.github.com>
2021-01-27 00:03:31 +0700

    Update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { join } from 'path';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  getHello(): string{
+    return join(__dirname, '..', '/views/partials');
+   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify'
 import { join } from 'path';
+import * as handlebars from 'handlebars';
+import * as hbs from 'hbs';
+import * as fs from 'fs'
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule,new FastifyAdapter(),);
@@ -19,6 +23,11 @@ async function bootstrap() {
     templates: join(__dirname, '..', 'views'),
   });
 
+  handlebars.registerPartial('header',handlebars.compile(fs.readFileSync(join(__dirname, '..', 'views/partials/header.hbs'), 'utf-8')));
+  handlebars.registerPartial('main',handlebars.compile(fs.readFileSync(join(__dirname, '..', 'views/partials/main.hbs'), 'utf-8')));
+  handlebars.registerPartial('footer',handlebars.compile(fs.readFileSync(join(__dirname, '..', 'views/partials/footer.hbs'), 'utf-8')));
+
+ // hbs.registerPartial(join(__dirname, "../", "/views/partials"));
   await app.listen(3000 , '0.0.0.0');
 }
 bootstrap();
