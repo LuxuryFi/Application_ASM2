faa076411652ffd66793ba2c04e511934332050a
Nguyễn Quốc Anh <65543110+LuxuryFi@users.noreply.github.com>
2021-01-27 00:16:35 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify'
 import { join } from 'path';
 import * as handlebars from 'handlebars';
 import * as hbs from 'hbs';
-import * as fs from 'fs'
-
+import * as fs from 'fs';
+import * as layouts from 'handlebars-layouts'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule,new FastifyAdapter(),);
@@ -26,6 +26,9 @@ async function bootstrap() {
   handlebars.registerPartial('header',handlebars.compile(fs.readFileSync(join(__dirname, '..', 'views/partials/header.hbs'), 'utf-8')));
   handlebars.registerPartial('main',handlebars.compile(fs.readFileSync(join(__dirname, '..', 'views/partials/main.hbs'), 'utf-8')));
   handlebars.registerPartial('footer',handlebars.compile(fs.readFileSync(join(__dirname, '..', 'views/partials/footer.hbs'), 'utf-8')));
+  handlebars.registerPartial('layout',handlebars.compile(fs.readFileSync(join(__dirname, '..', 'views/layouts.hbs'), 'utf-8')));
+
+  handlebars.registerHelper(layouts(handlebars));
 
  // hbs.registerPartial(join(__dirname, "../", "/views/partials"));
   await app.listen(3000 , '0.0.0.0');
